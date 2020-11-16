8f104e040618f15d9d052c22e384a437444cd001
Anton Knyazev <anton.knyazev@omega-r.com>
2020-11-16 17:19:03 +0300

    Fix display and view extensions

--- extensionslib/src/main/java/com/omega_r/libs/extensions/metrics/DensityExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/metrics/DensityExtensions.kt
@@ -5,3 +5,7 @@ import android.content.res.Resources
 fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
 
 fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun Float.toDp(): Float = (this / Resources.getSystem().displayMetrics.density)
+
+fun Float.toPx(): Float = (this * Resources.getSystem().displayMetrics.density)
--- extensionslib/src/main/java/com/omega_r/libs/extensions/view/ViewExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/view/ViewExtensions.kt
@@ -1,9 +1,35 @@
 package com.omega_r.libs.extensions.view
 
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
+import androidx.annotation.*
+import com.omega_r.libs.extensions.context.*
 
 fun View.inflate(@LayoutRes resource: Int, root: ViewGroup): View {
     return View.inflate(context, resource, root)
+}
+
+fun View.getCompatDrawable(@DrawableRes id: Int): Drawable? {
+    return context.getCompatDrawable(id)
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun View.getAnimatedVectorDrawable(@DrawableRes res: Int): AnimatedVectorDrawable? {
+    return context.getAnimatedVectorDrawable(res)
+}
+
+fun View.getCompatColor(@ColorRes id: Int): Int {
+    return context.getCompatColor(id)
+}
+
+fun View.getColorByAttribute(@AttrRes attrInt: Int): Int {
+    return context.getColorByAttribute(attrInt)
+}
+
+fun View.getColorDrawableByAttribute(@AttrRes attrInt: Int): ColorDrawable {
+    return context.getColorDrawableByAttribute(attrInt)
 }
