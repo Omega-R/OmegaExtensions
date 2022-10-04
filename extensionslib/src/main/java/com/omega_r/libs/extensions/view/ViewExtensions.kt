7c69276e81aa337647bf27f286ad361a7a53b887
Anton Knyazev <anton.knyazev@omega-r.com>
2022-10-04 19:53:41 +0300

    Add extensions for view padding

--- extensionslib/src/main/java/com/omega_r/libs/extensions/view/ViewExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/view/ViewExtensions.kt
@@ -3,6 +3,7 @@ package com.omega_r.libs.extensions.view
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.core.view.ViewCompat
 
 fun View.inflate(@LayoutRes resource: Int, root: ViewGroup): View {
     return View.inflate(context, resource, root)
@@ -11,3 +12,41 @@ fun View.inflate(@LayoutRes resource: Int, root: ViewGroup): View {
 fun View.inflate(@LayoutRes resource: Int): View {
     return View.inflate(context, resource, null)
 }
+
+var View.paddingStartMutable: Int
+    get() = ViewCompat.getPaddingStart(this)
+    set(value) = setPaddingRelativeOverload(start = value)
+
+var View.paddingEndMutable: Int
+    get() = ViewCompat.getPaddingEnd(this)
+    set(value) = setPaddingRelativeOverload(end = value)
+
+var View.paddingTopMutable: Int
+    get() = paddingTop
+    set(value) = setPaddingOverload(top = value)
+
+var View.paddingBottomMutable: Int
+    get() = paddingBottom
+    set(value) = setPaddingOverload(bottom = value)
+
+var View.paddingMutable: Int
+    get() = paddingLeft
+    set(value) = setPaddingOverload(left = value)
+
+var View.paddingRightMutable: Int
+    get() = paddingRight
+    set(value) = setPaddingOverload(right = value)
+
+fun View.setPaddingOverload(
+    left: Int = paddingLeft,
+    top: Int = paddingTop,
+    right: Int = paddingRight,
+    bottom: Int = paddingBottom
+) = setPadding(left, top, right, bottom)
+
+fun View.setPaddingRelativeOverload(
+    start: Int = ViewCompat.getPaddingStart(this),
+    top: Int = paddingTop,
+    end: Int = ViewCompat.getPaddingEnd(this),
+    bottom: Int = paddingBottom
+) = ViewCompat.setPaddingRelative(this, start, top, end, bottom)
