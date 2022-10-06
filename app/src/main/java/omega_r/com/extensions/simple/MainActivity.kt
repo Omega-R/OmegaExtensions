b18145c455a4d9bb29762d0c7447664c985b478b
Anton Knyazev <anton.knyazev@omega-r.com>
2022-10-06 16:16:56 +0300

    Fix compilations

--- app/src/main/java/omega_r/com/extensions/simple/MainActivity.kt
+++ app/src/main/java/omega_r/com/extensions/simple/MainActivity.kt
@@ -16,36 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(Dispatchers.Default).apply {
-            launchDelayed(4900) {
-                Log.d("MainActivity", "launchDelayed(4900)")
-            }
-            launchDelayed(5.seconds) {
-                Log.d("MainActivity", "launchDelayed(5.seconds)")
-            }
-            launchWithTicker(
-                ticksCount = 10,
-                tickDurationMillis = 1000,
-                initialDelayMillis = 5000,
-                onTick = {
-                    Log.d("MainActivity", "launchWithTicker($it)")
-                },
-                onEnd = {
-                    Log.d("MainActivity", "launchWithTicker(end)")
-                }
-            )
-            launchWithTicker(
-                ticksCount = 10,
-                tickDuration = 1.seconds,
-                initialDelay = 15.seconds,
-                onTick = {
-                    Log.d("MainActivity", "launchWithTicker2($it)")
-                },
-                onEnd = {
-                    Log.d("MainActivity", "launchWithTicker2(end)")
-                }
-            )
-        }
+
     }
 
 }
