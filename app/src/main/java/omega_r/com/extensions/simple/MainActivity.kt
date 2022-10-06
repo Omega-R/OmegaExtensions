e0e290de4c688b1c9a220821ac77a2ff17d7a8ce
Anton Knyazev <anton.knyazev@omega-r.com>
2022-10-06 16:47:49 +0300

    Set Deprecated old fun

--- app/src/main/java/omega_r/com/extensions/simple/MainActivity.kt
+++ app/src/main/java/omega_r/com/extensions/simple/MainActivity.kt
@@ -1,21 +1,14 @@
 package omega_r.com.extensions.simple
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.omega_r.libs.extensions.coroutines.launchDelayed
-import com.omega_r.libs.extensions.coroutines.launchWithTicker
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlin.time.DurationUnit
-import kotlin.time.seconds
+import androidx.appcompat.app.AppCompatActivity
+import com.omega_r.libs.extensions.string.removeNonDigit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 
--- extensionslib/src/main/java/com/omega_r/libs/extensions/string/DigitExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/string/DigitExtensions.kt
@@ -6,3 +6,9 @@ private val DIGIT_REGEX = Regex("[^0-9]")
 fun String.removeAllWithoutDigitAndLetters() = replace(NON_DIGIT_REGEX, "")
 
 fun String.removeNonDigit() = replace(DIGIT_REGEX, "")
+
+@Deprecated(message = "Use removeAllWithoutDigitAndLetters instead.", replaceWith = ReplaceWith("this.removeAllWithoutDigitAndLetters()", "com.omega_r.libs.extensions.string.removeAllWithoutDigitAndLetters"))
+fun String?.replaceNonDigit() = this?.removeAllWithoutDigitAndLetters()
+
+@Deprecated(message = "", replaceWith = ReplaceWith("this.removeNonDigit()"))
+fun String?.replaceDigit() = this?.removeNonDigit()
