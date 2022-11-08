2ff0c0443aa7092b302f1c4316d313f680b2c263
Anton Knyazev <anton.knyazev@omega-r.com>
2022-11-08 10:42:27 +0300

    Add canvas extensions Fix drawable extensions

--- extensionslib/src/main/java/com/omega_r/libs/extensions/canvas/CanvasExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/canvas/CanvasExtensions.kt
@@ -2,6 +2,8 @@ package com.omega_r.libs.extensions.canvas
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 
 fun Canvas.drawCircle(cx: Int, cy: Int, radius: Int, paint: Paint) {
     drawCircle(cx.toFloat(), cy.toFloat(), radius.toFloat(), paint)
@@ -9,4 +11,19 @@ fun Canvas.drawCircle(cx: Int, cy: Int, radius: Int, paint: Paint) {
 
 fun Canvas.drawCircle(cx: Int, cy: Int, radius: Float, paint: Paint) {
     drawCircle(cx.toFloat(), cy.toFloat(), radius, paint)
+}
+
+fun Canvas.drawDrawable(left: Float, top: Float, right: Float, bottom: Float, drawable: Drawable) {
+    drawable.setBounds(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+    drawable.draw(this)
+}
+
+fun Canvas.drawDrawable(left: Int, top: Int, right: Int, bottom: Int, drawable: Drawable) {
+    drawable.setBounds(left, top, right, bottom)
+    drawable.draw(this)
+}
+
+fun Canvas.drawDrawable(rect: Rect, drawable: Drawable) {
+    drawable.bounds = rect
+    drawable.draw(this)
 }
--- extensionslib/src/main/java/com/omega_r/libs/extensions/drawable/DrawableExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/drawable/DrawableExtensions.kt
@@ -1,9 +1,6 @@
 package com.omega_r.libs.extensions.drawable
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.ScaleDrawable
 import android.view.Gravity.NO_GRAVITY
@@ -17,33 +14,17 @@ import com.omega_r.libs.extensions.context.getCompatDrawable
 fun Context.getCompatDrawable(@DrawableRes id: Int, @ColorRes color: Int): Drawable? =
     getCompatDrawable(id)?.tint(this, color)
 
-fun Drawable?.tint(context: Context, @ColorRes color: Int): Drawable? =
-    this?.tint(ContextCompat.getColor(context, color))
+fun Drawable.tint(context: Context, @ColorRes color: Int): Drawable =
+    tint(ContextCompat.getColor(context, color))
 
-fun Drawable?.tint(@ColorInt color: Int): Drawable? {
-    return this?.let {
-        val mutate = DrawableCompat.wrap(it).mutate()
-        DrawableCompat.setTint(mutate, color)
-        return@let mutate
+fun Drawable.tint(@ColorInt color: Int): Drawable {
+    return DrawableCompat.wrap(this).mutate().apply {
+        DrawableCompat.setTint(this, color)
     }
 }
 
-fun Drawable?.scale(resources: Resources, size: Int) : Drawable? = this?.scale(resources, size, size)
+fun Drawable.scale(size: Int): Drawable = scale(size, size)
 
-fun Drawable?.scale(resources: Resources, height: Int, width: Int) : Drawable? {
-    if (this == null) return null
-    if (this is BitmapDrawable) {
-        bitmap?.let {
-            val bitmap: Bitmap? = Bitmap.createScaledBitmap(it, height, width, true)
-            if (bitmap != null) return BitmapDrawable(resources, bitmap)
-        }
-    }
-
-    val scaleDrawable = ScaleDrawable(this, NO_GRAVITY, width.toFloat(), height.toFloat()).drawable
-    scaleDrawable?.let {
-        it.setBounds(0, 0, width, height)
-        return it
-    }
-
-    return this
+fun Drawable.scale(height: Int, width: Int): Drawable {
+    return ScaleDrawable(this, NO_GRAVITY, width.toFloat(), height.toFloat())
 }
