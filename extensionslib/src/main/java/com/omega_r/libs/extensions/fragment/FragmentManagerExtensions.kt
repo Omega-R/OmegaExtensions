d36e7072002accf6c34935398d0b580d6872355d
Anton Knyazev <anton.knyazev@omega-r.com>
2023-11-13 10:04:45 +0300

    Fix FragmentManager.edit

--- extensionslib/src/main/java/com/omega_r/libs/extensions/fragment/FragmentManagerExtensions.kt
+++ extensionslib/src/main/java/com/omega_r/libs/extensions/fragment/FragmentManagerExtensions.kt
@@ -3,8 +3,27 @@ package com.omega_r.libs.extensions.fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
-inline fun FragmentManager.edit(block: FragmentTransaction.() -> Unit) {
+inline fun FragmentManager.edit(
+    commitNow: Boolean = false,
+    allowingStateLoss: Boolean = false,
+    block: FragmentTransaction.() -> Unit
+) {
     beginTransaction()
         .apply(block)
-        .commit()
+        .apply {
+            commitAllowingStateLoss()
+            if (commitNow) {
+                if (allowingStateLoss) {
+                    commitNowAllowingStateLoss()
+                } else {
+                    commitNow()
+                }
+            } else {
+                if (allowingStateLoss) {
+                    commitAllowingStateLoss()
+                } else {
+                    commit()
+                }
+            }
+        }
 }
